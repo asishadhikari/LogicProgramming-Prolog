46e29dfab8caac49947674644e0fbbd9dff0f9ab
sikari <ashish.adhikari@bison.howard.edu>
2017-02-07 20:52:23 -0500

    finalize

--- lp.pl
+++ lp.pl
@@ -1,11 +1,20 @@
 
-%if list exhausted return 0
 %['C:/Users/asish/Desktop/LogicProgramming-Prolog/lp.pl'].
 
+%question 1 
+%if list exhausted return 0
 sum-up-numbers-simple([],0).
 
+%sum for general case
 sum-up-numbers-simple(L,N):-
 	[X|Y] = L,
-	sum-up-numbers-simple(Y,N1),  
-	N is (X + N1).
-	
+	sum-up-numbers-simple(Y,N1),
+	N is X + N1.
+
+sum-up-numbers-simple(
+
+%question 2
+
+sum-up-numbers-general(L, N)
+
+
