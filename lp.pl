d6f43988311447c30e7bd75d8161cdc556470197
sikari <ashish.adhikari@bison.howard.edu>
2017-02-07 19:51:50 -0500

    added more cases

--- lp.pl
+++ lp.pl
@@ -1,6 +1,11 @@
+
+%if list exhausted return 0
 sum-up-numbers-simple([],0).
+
 
 sum-up-numbers-simple(L,N):-
 	[X|Y] = L,
-	[X1|y1] = Y,  
+	[X1|y1] = Y,
+	sum-up-numbers-simple (X1  
+	N is X + Y1.
 	
